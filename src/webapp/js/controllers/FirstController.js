176ab7223a9ea6c249c47fc3c80607c899076c51
Bas Biesheuvel <bas.biesheuvel@ing.nl>
2014-11-28 12:37:00 +0100

    Added timeout around synchronous call.

--- src/webapp/js/controllers/FirstController.js
+++ src/webapp/js/controllers/FirstController.js
@@ -1,29 +1,33 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular.module('j.point.me').controller('FirstController', ['$scope', '$firebase', '$firebaseAuth',
-    function ($scope, $firebase, $firebaseAuth) {
+    angular.module('j.point.me').controller('FirstController', ['$scope', '$firebase', '$firebaseAuth', '$timeout',
+        function ($scope, $firebase, $firebaseAuth, $timeout) {
 
-      var ref = new Firebase("https://jpointme.firebaseio.com/");
+            var ref = new Firebase("https://jpointme.firebaseio.com/");
+            $scope.username = "anonymous";
 
-      $scope.username = "anonymous";
-      var auth = ref.getAuth();
-      if (!auth) {
-        var auth = $firebaseAuth(ref);
-        auth.$authWithOAuthRedirect("github").then(function(authData) {
-          console.log("Logged in as:", authData);
+            // Use timeout to prevent the synchronous call to hold the controller initialization
+            $timeout(function () {
 
-        }).catch(function(error) {
-          console.error("Authentication failed: ", error);
-        });
-      }
+                var auth = ref.getAuth();
+                if (!auth) {
+                    var auth = $firebaseAuth(ref);
+                    auth.$authWithOAuthRedirect("github").then(function (authData) {
+                        console.log("Logged in as:", authData);
 
-      $scope.username = auth ? auth.github.displayName : "anonymous";
+                    }).catch(function (error) {
+                        console.error("Authentication failed: ", error);
+                    });
+                }
 
-      // create an AngularFire reference to the data
-      var sync = $firebase(ref);
-      // download the data into a local object
-      $scope.data = sync.$asObject();
-    }
-  ]);
+                $scope.username = auth ? auth.github.displayName : "anonymous";
+
+                // create an AngularFire reference to the data
+                var sync = $firebase(ref);
+                // download the data into a local object
+                $scope.data = sync.$asObject();
+            }, 10);
+        }
+    ]);
 }());
