467a4de007e9ee1d3ca4c3651ee61a767e03e532
Tim van Eijndhoven <tvaneijndhoven@jpoint.nl>
2014-11-28 13:15:02 +0100

    Add buttons for logon and logoff

--- src/webapp/js/controllers/FirstController.js
+++ src/webapp/js/controllers/FirstController.js
@@ -1,33 +1,27 @@
 (function () {
     'use strict';
 
-    angular.module('j.point.me').controller('FirstController', ['$scope', '$firebase', '$firebaseAuth', '$timeout',
-        function ($scope, $firebase, $firebaseAuth, $timeout) {
+    angular.module('j.point.me').controller('FirstController', ['$scope', '$firebase', '$firebaseAuth',
+        function ($scope, $firebase, $firebaseAuth) {
 
             var ref = new Firebase("https://jpointme.firebaseio.com/");
             $scope.username = "anonymous";
 
-            // Use timeout to prevent the synchronous call to hold the controller initialization
-            $timeout(function () {
+            $scope.authenticate = function(provider) {
+              var auth = $firebaseAuth(ref);
+              auth.$authWithOAuthPopup("github").then(function(authData) {
+                console.log("Logged in as:", authData);
+                $scope.username = authData.github.displayName;
+              }).catch(function(error) {
+                console.error("Authentication failed: ", error);
+              });
+            };
 
-                var auth = $firebaseAuth(ref);
-                console.log('loading controller', auth.$getAuth());
-
-                if (!auth.$getAuth()) {
-
-                    auth.$authWithOAuthPopup("github").then(function (authData) {
-                        console.log("Logged in as:", authData.github.displayName);
-
-                        $scope.username = authData ? authData.github.displayName : "anonymous";
-
-                    }).catch(function (error) {
-                        console.error("Authentication failed: ", error);
-                    });
-                } else {
-                    $scope.username = auth.$getAuth().github.displayName;
-                }
-
-            }, 10);
+            $scope.logout = function() {
+              ref.unauth().then(function() {
+                $scope.username = "anonymous";
+              });
+            };
         }
     ]);
 }());
