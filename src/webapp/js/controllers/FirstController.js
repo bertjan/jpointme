49268dac83744bdbad2251f4c46dc6ac368a5a2c
Tim van Eijndhoven <tvaneijndhoven@jpoint.nl>
2014-11-28 12:13:07 +0100

    Add github authentication

--- src/webapp/js/controllers/FirstController.js
+++ src/webapp/js/controllers/FirstController.js
@@ -1,12 +1,25 @@
 (function () {
   'use strict';
 
-  angular.module('j.point.me').controller('FirstController', ['$scope', '$firebase',
-    function ($scope, $firebase) {
-
-      $scope.text = "Hello world.";
+  angular.module('j.point.me').controller('FirstController', ['$scope', '$firebase', '$firebaseAuth',
+    function ($scope, $firebase, $firebaseAuth) {
 
       var ref = new Firebase("https://jpointme.firebaseio.com/");
+
+      $scope.username = "anonymous";
+      var auth = ref.getAuth();
+      if (!auth) {
+        var auth = $firebaseAuth(ref);
+        auth.$authWithOAuthRedirect("github").then(function(authData) {
+          console.log("Logged in as:", authData);
+
+        }).catch(function(error) {
+          console.error("Authentication failed: ", error);
+        });
+      }
+
+      $scope.username = auth ? auth.github.displayName : "anonymous";
+
       // create an AngularFire reference to the data
       var sync = $firebase(ref);
       // download the data into a local object
